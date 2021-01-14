3e39a531eac3de3487e748b132544c73194c5914
MhemAungThu <kweesai.saitama@gmail.com>
2021-01-14 08:28:09 +0630

    refactor: add `add_toml` function
    Create the `read_toml` function which return `Result<T, E> for error handling.
    The `main` function called read toml and make error handling.
    This project is refactor the code from the Chapter 2 of the Creative Projects
    for Rust Programmers book.

---
+++ tomldyn/src/main.rs
@@ -0,0 +1,38 @@
+use std::env;
+use std::process;
+
+fn main() {
+    let config_const_values = read_toml(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing argument: {}", err);
+        process::exit(1);
+    });
+
+    println!("Original: {:#?}", config_const_values);
+    println!(
+        "[Postgresql].Database: {}",
+        config_const_values
+            .get("postgresql")
+            .unwrap()
+            .get("database")
+            .unwrap()
+            .as_str()
+            .unwrap()
+    );
+}
+
+// refactoring code according to the
+fn read_toml(mut args: env::Args) -> Result<toml::Value, &'static str> {
+    args.next();
+
+    let filename = match args.next() {
+        Some(arg) => arg,
+        None => return Err("Didn't get a filename"),
+    };
+
+    let config_text = match std::fs::read_to_string(&filename) {
+        Ok(text) => text,
+        Err(_) => return Err("File not found"),
+    };
+
+    Ok(config_text.parse::<toml::Value>().unwrap())
+}
